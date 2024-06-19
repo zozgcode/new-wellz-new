5a165b891375ca50c4abdb87b24d1b05168f7fd2
sunshine-web-coder <webdev4gaming@gmail.com>
2024-06-19 14:58:57 +0100

    first commit

---
+++ src/components/mockData/MockData.ts
@@ -0,0 +1,138 @@
+import { Account } from "@/utils/types";
+
+export const mockAccounts: Account[] = [
+  {
+    account_id: "ACC001",
+    holder: {
+      firstName: "James",
+      lastName: "Radford",
+      jointNames: "",
+      username: "sample1",
+      password: "sample1",
+    },
+    bank_details: {
+      account_type: "Checking",
+      balance_usd: 2500000.0,
+    },
+    transaction_mgs: {
+      transaction_text_msg: "",
+    },
+    transaction_history: [
+      {
+        transaction_id: "TXN001",
+        dateTime: "March 23rd 2024, 3:32:32 am",
+        description: "Sinlair's Store - Trx",
+        status: "Success",
+        amount_usd: 452.0,
+        account_no: "****5009",
+      },
+      {
+        transaction_id: "TXN002",
+        dateTime: "March 20th 2024, 2:02:22 am",
+        description: "Justin Loci",
+        status: "Success",
+        amount_usd: 1152.21,
+        account_no: "****7676",
+      },
+      {
+        transaction_id: "TXN003",
+        dateTime: "March 20th 2024, 2:02:22 am",
+        description: "Transfer",
+        status: "Success",
+        amount_usd: -100.0,
+        account_no: "****4409",
+      },
+      {
+        transaction_id: "TXN004",
+        dateTime: "March 20th 2024, 2:02:22 am",
+        description: "Payment",
+        status: "Success",
+        amount_usd: -50.0,
+        account_no: "****5093",
+      },
+      {
+        transaction_id: "TXN005",
+        dateTime: "March 20th 2024, 2:02:22 am",
+        description: "Online Purchase",
+        status: "Success",
+        amount_usd: -75.0,
+        account_no: "****2334",
+      },
+      {
+        transaction_id: "TXN006",
+        dateTime: "March 20th 2024, 2:02:22 am",
+        description: "ATM Withdrawal",
+        status: "Success",
+        amount_usd: -150.0,
+        account_no: "****3213",
+      },
+      {
+        transaction_id: "TXN007",
+        dateTime: "March 20th 2024, 2:02:22 am",
+        description: "Transfer",
+        status: "Pending",
+        amount_usd: 200.0,
+        account_no: "****4434",
+      },
+    ],
+  },
+  {
+    account_id: "ACC002",
+    holder: {
+      firstName: "Lynn & Graham",
+      lastName: "",
+      jointNames: "Lynn Nolan Bishop & Graham Linda",
+      username: "bishoplinda",
+      password: "honesty21",
+    },
+    bank_details: {
+      account_type: "Checking",
+      balance_usd: 3700000.0,
+    },
+    transaction_mgs: {
+      transaction_text_msg: "WTC WIRE TRANSFER CODE",
+    },
+    transaction_history: [
+      {
+        transaction_id: "TXN001",
+        dateTime: "June 30 2023, 3:32:32 am",
+        description: "WIRE TRANSFER",
+        status: "Success",
+        amount_usd:580000.0,
+        account_no: "****5009",
+      },
+      {
+        transaction_id: "TXN002",
+        dateTime: "April 15 2023, 1:04:02 pm",
+        description: "Partnership Funding",
+        status: "Success",
+        amount_usd: 250000.0,
+        account_no: "****5009",
+      },
+      {
+        transaction_id: "TXN003",
+        dateTime: "February 8 2023, 10:11:22 am",
+        description: "Refund",
+        status: "Success",
+        amount_usd: 650000.0,
+        account_no: "****5009",
+      },
+      {
+        transaction_id: "TXN004",
+        dateTime: "December 2 2022, 7:08:32 am",
+        description: "Insurance Claim",
+        status: "Success",
+        amount_usd: 175000.0,
+        account_no: "****5009",
+      },
+      {
+        transaction_id: "TXN005",
+        dateTime: "March 23 2022, 7:15:02 pm",
+        description: "Project Milestone Payment",
+        status: "Success",
+        amount_usd: 965000.0,
+        account_no: "****5009",
+      },
+    ],
+  },
+];
